6871e3e5e84a36efde6a0b28126433a46bb44d9a
Aiden <aylatif182@gmail.com>
2026-08-18 23:26:47 -0400

    add ball bouncing and losing stuff

--- pong.py
+++ pong.py
@@ -16,6 +16,11 @@ black = (0, 0, 0)
 paddle_speed = 6
 clock = pygame.time.Clock()
 
+Lost = False
+Lost_player1 = False
+Lost_player2 = False
+player_score = 0
+
 class paddle:
     def __init__(self, pos_x, pos_y, pos_x_end, pos_y_end, up_key, down_key):
         self.pos_x = pos_x
@@ -47,15 +52,29 @@ class ball:
         self.y_pick = random.choice(self.y_list)
 
     def spawn(self):
-        pygame.draw.circle(display_surf, white, (self.pos_x, self.pos_y), self.radius)
-        
+        pygame.draw.rect(display_surf, white, (self.pos_y, self.pos_x, 5, 5))
+
     def move(self):
-        self.pos_x += self.x_pick
-        self.pos_y += self.y_pick
+        self.pos_x += self.x_pick * 1.5
+        self.pos_y += self.y_pick * 1.5
+
+    def bounce(self):
+        global Lost_player1
+        global Lost_player2
+        global Lost
+        if self.pos_x <= 0 or self.pos_x >= DISPLAY_HEIGHT:
+            self.x_pick = self.x_pick * -1
+        
+        if self.pos_y <= 0:
+            Lost_player1 = True
+            Lost = True
+        elif self.pos_y >= DISPLAY_WIDTH:
+            Lost_player2 = True
+            Lost = True
 
 paddle_game = paddle(1,200,1,400,pygame.K_w,pygame.K_s)
 paddle_game_2 = paddle(798,200,798,400,pygame.K_UP,pygame.K_DOWN)
-ball_game = ball(400, 300, 5)
+ball_game = ball(300, 400, 5)
 
 running = True
 while running:
@@ -66,6 +85,7 @@ while running:
     keys = pygame.key.get_pressed()
 
     ball_game.move()
+    ball_game.bounce()
 
     paddle_game.move(keys,paddle_speed)
     paddle_game_2.move(keys,paddle_speed)
@@ -74,6 +94,22 @@ while running:
     paddle_game.draw()
     paddle_game_2.draw()
     ball_game.spawn()
+
+    if Lost == True:
+        overlay = pygame.Surface((display_surf.get_width(), display_surf.get_height()))
+        overlay.set_alpha(180)
+        overlay.fill((30, 30, 30))
+        display_surf.blit(overlay, (0, 0))
+        if keys[pygame.K_r]:
+            new_game()
+
+        font = pygame.font.Font(None, 74)
+        if Lost_player1 == True:
+            text_surface = font.render(f"Player 1 lost\nPress R to restart", True, (255, 255, 255))
+        elif Lost_player2:
+            text_surface = font.render(f"Player 2 lost\nPress R to restart", True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(display_surf.get_width() // 2, display_surf.get_height() // 2))
+        display_surf.blit(text_surface, text_rect)
 
     pygame.display.flip()
     clock.tick(60)
